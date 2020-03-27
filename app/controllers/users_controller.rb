e238c93f4f1565c03263c43f140f41f5f8a19d5e
pirainogi <pirainogi@gmail.com>
2020-03-27 09:21:18 -0400

    added bcrypt, can create user, not sure why user show is brooken

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authorize, only: [:show, :new]
 
   # GET /users
   # GET /users.json
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @investors = @user.investors.all
   end
 
   # GET /users/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'startup/index'
   resources :users
   resources :investors
+
+
+  get '/login', to: "sessions#login", as: "login"
+  post '/sessions', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
