78db7be910ee300f25cb7a596d6f1aaa751e10b0
pirainogi <pirainogi@gmail.com>
2020-03-27 12:39:34 -0400

    redirect if user saved in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username])
-    byebug
     if !!@user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
