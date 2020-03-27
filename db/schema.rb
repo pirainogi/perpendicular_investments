5ea520c2acfc9a9636b120cab0598add0b1eec6a
pirainogi <pirainogi@gmail.com>
2020-03-27 11:30:47 -0400

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_125625) do
+ActiveRecord::Schema.define(version: 2020_03_27_152752) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
